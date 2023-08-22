fd04c49c3a89eb3ef2358a79dd855c0428603a7a
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-08-22 23:41:35 +0100

    test: add endpoints tests

---
+++ test/app.test.js
@@ -0,0 +1,75 @@
+import request from 'supertest';
+import app from '../src/app';
+
+describe('/login endpoint testing suite', () => {
+  it('POST should return 200 when succesful login', async () => {
+    const response = await request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({ user: 'kevin', password: '1234' });
+
+    expect(response.status).toBe(200);
+  });
+
+  it('POST should return 400 when no data provided', async () => {
+    const response = await request(app).post('/login');
+
+    expect(response.status).toBe(400);
+  });
+
+  it('POST should return 400 when no username provided', async () => {
+    const response = await request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({ password: '1234' });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('POST should return 400 when no password provided', async () => {
+    const response = await request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({ user: 'kevin' });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('POST should return 401 when invalid credentials provided', async () => {
+    const response = await request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({ user: 'kevin', password: '12' });
+
+    expect(response.status).toBe(401);
+  });
+});
+
+describe('/ endpoint testing suite', () => {
+  it('GET should return 200 when jwt is valid', async () => {
+    const loginResponse = await request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({ user: 'kevin', password: '1234' });
+
+    const response = await request(app)
+      .get('/')
+      .set('Authorization', `JWT ${loginResponse.body.token}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('GET should return 401 when no jwt provided', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('GET should return 401 when invalid jwt provided', async () => {
+    const response = await request(app)
+      .get('/')
+      .set('Authorization', 'JWT invalidToken');
+
+    expect(response.status).toBe(401);
+  });
+});
