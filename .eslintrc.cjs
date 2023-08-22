b11eef4d61ccfa24163b680cf010c429c3181573
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-08-22 23:37:57 +0100

    chore: set up husky hooks

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/extensions': 'off',
+  },
+};
