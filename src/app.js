98f2b4a5894f99362f1c79d2703b00c0b09dc33b
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-08-28 20:19:25 +0100

    feat: implement auth routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import passport from 'passport';
-import jwt from 'jsonwebtoken';
 import bodyParser from 'body-parser';
 import authConfig from './auth.js';
-import { registerUser, checkUserCredentials } from './controllers/users.js';
-
-await registerUser('kevin', '1234');
+import authRoutes from './routers/auth.js';
 
 authConfig(passport);
 
@@ -16,20 +13,6 @@ app.get('/', passport.authenticate('jwt', { session: false }), (_req, res) => {
   res.send('hello world');
 });
 
-app.post('/login', async (req, res) => {
-  if (!req.body.user || !req.body.password) {
-    return res.status(400).json({ message: 'Missing data' });
-  }
-
-  const validCredentials = await checkUserCredentials(req.body.user, req.body.password);
-
-  if (!validCredentials) {
-    return res.status(401).json({ message: 'Invalid credentials' });
-  }
-
-  const token = jwt.sign({ userId: req.body.user }, 'secretPassword');
-
-  return res.status(200).json({ token });
-});
+app.use('/auth', authRoutes);
 
 export default app;
