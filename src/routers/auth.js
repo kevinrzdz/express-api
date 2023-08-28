c19dbac7e11857c8d1a14b611958a19b261b2720
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-08-28 20:17:27 +0100

    feat: add auth router

---
+++ src/routers/auth.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import { checkUserCredentials, registerUser } from '../controllers/users.js';
+
+const router = express.Router();
+await registerUser('kevin', '1234');
+
+router.route('/')
+  .get((_req, res) => res.send('GET Auth router')
+    .post((_request, response) => response.send('POST Auth router')));
+
+router.route('/login')
+  .post(async (req, res) => {
+    if (!req.body.user || !req.body.password) {
+      return res.status(400)
+        .json({ message: 'Missing data' });
+    }
+
+    const validCredentials = await checkUserCredentials(req.body.user, req.body.password);
+
+    if (!validCredentials) {
+      return res.status(401)
+        .json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: req.body.user }, 'secretPassword');
+
+    return res.status(200)
+      .json({ token });
+  });
+
+export default router;
