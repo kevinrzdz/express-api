e58cc7ff5a3d3259843cbc4af1deec84e5d72ca5
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-09-06 20:05:03 +0100

    feat: add auth testing

---
+++ src/auth/test/auth.test.js
@@ -0,0 +1,66 @@
+import request from 'supertest';
+import { app, server } from '../../app.js';
+import { clearUsers, registerUser } from '../users.js';
+
+beforeAll(() => registerUser('kevin', '1234'));
+
+describe('/login endpoint testing suite', () => {
+  it('POST should return 200 when successful login', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('content-type', 'application/json')
+      .send({
+        user: 'kevin',
+        password: '1234',
+      });
+
+    expect(response.status)
+      .toBe(200);
+  });
+
+  it('POST should return 400 when no data provided', async () => {
+    const response = await request(app)
+      .post('/auth/login');
+
+    expect(response.status)
+      .toBe(400);
+  });
+
+  it('POST should return 400 when no username provided', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('content-type', 'application/json')
+      .send({ password: '1234' });
+
+    expect(response.status)
+      .toBe(400);
+  });
+
+  it('POST should return 400 when no password provided', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('content-type', 'application/json')
+      .send({ user: 'kevin' });
+
+    expect(response.status)
+      .toBe(400);
+  });
+
+  it('POST should return 401 when invalid credentials provided', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('content-type', 'application/json')
+      .send({
+        user: 'kevin',
+        password: '12',
+      });
+
+    expect(response.status)
+      .toBe(401);
+  });
+});
+
+afterAll((done) => {
+  clearUsers();
+  server.close(done);
+});
