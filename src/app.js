9015e26cef3869165f6dabdfa499afc406a0d04a
Kevin Ruiz <kevin.rf.es@gmail.com>
2023-09-06 20:28:36 +0100

    feat: add middleware config call on app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 import express from 'express';
-import passport from 'passport';
-import bodyParser from 'body-parser';
-import authConfig from './auth/auth.js';
 import authRoutes from './auth/auth-router.js';
-
-authConfig(passport);
+import setUpMiddlewares from './tools/middlewares.js';
 
 const app = express();
-app.use(bodyParser.json());
 
 const PORT = process.env.PORT ?? 0;
 
-app.get('/', passport.authenticate('jwt', { session: false }), (_req, res) => {
+setUpMiddlewares(app);
+
+app.get('/', (_req, res) => {
   res.send('hello world');
 });
 
