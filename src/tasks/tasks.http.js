f26c0b130a22718821bd390e7cc483f01c5c283b
Kevin Ruiz Díaz <kevin.rf.es@gmail.com>
2024-04-14 10:18:59 +0100

    refactor: Rename tasksHttp to tasksController in tasks.http.js
    This commit updates the import name and usage of the tasks.controller.js module in tasks.http.js. The old name, tasksHttp, was replaced with tasksController for better clarity and to align with the module's actual functionality. All associated methods have been updated to use the new name.

--- src/tasks/tasks.http.js
+++ src/tasks/tasks.http.js
@@ -1,4 +1,4 @@
-import * as tasksHttp from './tasks.controller.js';
+import * as tasksController from './tasks.controller.js';
 import { getUser } from '../auth/users.controller.js';
 
 const getAllTasks = (req, res) => {
@@ -7,14 +7,14 @@ const getAllTasks = (req, res) => {
 
   res.json({
     user: user.username,
-    tasks: tasksHttp.getAllTasks(userId),
+    tasks: tasksController.getAllTasks(userId),
   });
 };
 
 const getTask = (req, res) => {
   const { userId } = req.user;
   const { taskId } = req.params;
-  const task = tasksHttp.getTask(userId, taskId);
+  const task = tasksController.getTask(userId, taskId);
 
   if (!task) {
     res.status(404)
@@ -27,7 +27,7 @@ const getTask = (req, res) => {
 const addTask = (req, res) => {
   const { userId } = req.user;
   const { name } = req.body;
-  const newTask = tasksHttp.addTask(userId, name);
+  const newTask = tasksController.addTask(userId, name);
 
   res.json({ task: newTask });
 };
@@ -36,7 +36,7 @@ const setTasks = (req, res) => {
   const { userId } = req.user;
   const { tasks: tasksFromRequest } = req.body;
 
-  const updatedTasks = tasksHttp.setTasks(userId, tasksFromRequest);
+  const updatedTasks = tasksController.setTasks(userId, tasksFromRequest);
 
   res.json({ tasks: updatedTasks });
 };
@@ -45,7 +45,7 @@ const deleteTask = (req, res) => {
   const { userId } = req.user;
   const { taskId } = req.params;
 
-  tasksHttp.deleteTask(userId, taskId);
+  tasksController.deleteTask(userId, taskId);
 
   res.send();
 };
@@ -55,14 +55,14 @@ const editTask = (req, res) => {
   const { taskId } = req.params;
   const { name } = req.body;
 
-  const newTask = tasksHttp.editTask(userId, taskId, name);
+  const newTask = tasksController.editTask(userId, taskId, name);
 
   res.json({ task: newTask });
 };
 
 const resetTasks = (req, res) => {
   const { userId } = req.user;
-  tasksHttp.bootstrapTasks(userId);
+  tasksController.bootstrapTasks(userId);
   res.send();
 };
 
